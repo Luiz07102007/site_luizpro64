04c8d59f7d2f735aa09981f54fa52d505e79926b
Luiz07102007 <127757663+Luiz07102007@users.noreply.github.com>
2023-05-24 00:16:13 -0300

    Add files via upload

---
+++ LuizFelipe/javascript/script.js
@@ -0,0 +1,34 @@
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonImage = document.querySelector('.pokemon_image');
+
+const form =document.querySelector('.form');
+const input=document.querySelector('.input_search');
+
+const  fetchPokemon = async (pokemon) => {
+const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);    
+
+if (APIResponse.status = 200) {
+
+const data = await APIResponse.json();
+return data;
+}
+}
+
+const renderPokemon = async (pokemon) => {
+
+const data = await fetchPokemon(pokemon); 
+if (data) {
+pokemonName.innerHTML =data.name;
+pokemonNumber.innerHTML =data.id;
+pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']
+['front_default']
+input.value= '';
+}
+}
+form.addEventListener('submit', (event) => {
+
+    event.preventDefault();
+    renderPokemon(input.value);
+   
+});
